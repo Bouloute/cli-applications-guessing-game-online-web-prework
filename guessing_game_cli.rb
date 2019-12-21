6a159be6fc1e398ace7417ead3089b795abbc37f
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-12-21 16:00:38 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,12 @@
-puts("Goodbye!")
+random_number = rand(1..6)
+puts("Guess a number between 1 and 6")
+guessed_number = gets.chomp 
+
+case guessed_number
+when random_number == guessed_number
+  puts("You guessed the correct number!")
+when random_number != guessed_number
+  puts("Sorry! The computer guessed #{random_number}.")
+else
+  puts("Goodbye")
+end 
