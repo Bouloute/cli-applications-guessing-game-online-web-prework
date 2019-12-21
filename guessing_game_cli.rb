7de0b3817d747927094249476e4aa2fead124889
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-12-21 16:10:35 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,6 +9,6 @@ def run_guessing_game
   when "exit"
     puts("Goodbye!")
   else 
-    puts("Sorry! The computer guessed #{random_number} and you guessed #{guessed_number}.")
+    puts("Sorry! The computer guessed #{random_number}.")
   end 
 end
